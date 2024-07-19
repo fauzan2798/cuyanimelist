f3fada9eb0db846d768aa1519ea6dcba8cb49b39
Fauzan <fauzan.azhar2798@gmail.com>
2024-07-19 14:08:18 +0700

    add feature search

--- src/components/AnimeList/header.js
+++ src/components/AnimeList/header.js
@@ -4,7 +4,13 @@ const Header = ({ title, linkTitle, linkHref}) => {
     return (
         <div className="flex justify-between items-center p-4">
             <h1 className="font-bold text-2xl">{title}</h1>
-            <Link href={`${linkHref}`} className="md:text-xl text-sm underline hover:text-indigo-500 transition-all">{linkTitle}</Link>
+            {
+                linkHref && linkTitle
+                ?
+                <Link href={`${linkHref}`} className="md:text-xl text-sm underline hover:text-indigo-500 transition-all">{linkTitle}</Link>
+                :
+                null
+            }
         </div>
     )
 }
