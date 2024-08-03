ae59c25ef65bb9c6efd99baa6b71449923d5b893
risanjayy <risyadul@thelionparcel.com>
2024-08-03 22:08:28 +0700

    connecting mysql database with PRISMA ORM

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,15 +1,17 @@
 "use client"
 
 import { FileSearch } from "@phosphor-icons/react"
-import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const Page = () => {
+    const router = useRouter()
+
     return (
         <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
             <div className="flex justify-center items-center flex-col gap-4">
                 <FileSearch size={32} className="text-color-accent"/>
                 <h3 className="text-color-accent text-2xl font-bold">NOT FOUND</h3>
-                <Link href="/" className="text-color-primary hover:text-color-accent transition-all">kembali</Link>
+                <button onClick={() => router.back()} className="text-color-primary hover:text-color-accent transition-all">Kembali</button>
             </div>
         </div>
     )
